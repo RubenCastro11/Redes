02e6209d399c0b87bf56cdb9f2e97cdf36affee0
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2021-01-07 11:47:14 +0000

    sistema CRUD - read -details.

---
+++ projeto-filmes/filmes_show.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+
+</head>
+
+<?php
+if($_SERVER['REQUEST_METHOD']=="GET"){
+	if(!isset($_GET['filme']) || !is_numeric($_GET['filme'])){
+		echo '<script>alert("Erro ao abrir livro");</script>';
+		echo 'Aguarde um momento. A reencaminhar página';
+		header("refresh:5; url=index.php");
+		exite();
+	}
+	$idFilme=$_GET['filme'];
+	$con=new mysqli("localhost","root","","filmes");
+
+	if($con->connect_errno!=0){
+		echo 'Ocorreu um erro no acesso á base de dados. <br>'.$con->connect_error;
+		exit;
+	}
+	else{
+		$sql = 'select*from filmes where id_filme =?';
+		$stm = $con->prepare($sql);
+		if($stm!=false){
+			$stm->bind_param('i',$idFilme);
+			$stm->execute();
+			$res=$stm->get_result();
+			$livro=$res->fetch_assoc();
+			$stm->close();
+		}
+		else{
+			echo '<br>';
+			echo($con->error);
+			echo('<br>');
+			echo "Aguarde um momento.A reencaminhar página";
+			echo'<br>';
+			header("refresh:5; url=index.php");
+		}
+	}//end if-if($con->connect_ermo!=0)
+}//if($_SERVER['REQUEST_METHOD']=="GET")
+
+?>
+
+<body>
+	<h1>Detalhes do filme</h1>
+	<?php
+
+	if(isset($livro)){
+		echo'<br>';
+		echo $livro["titulo"];
+		echo '<br>';
+		echo $livro["sinopse"];
+		echo'<br>';
+		echo $livro["idioma"];
+		echo'<br>';
+		echo $livro["data_lancamento"];
+
+		echo'<br>';
+		echo $livro["quantidade"];
+		echo'<br>';
+	}
+	else{
+		echo'<h2>Parece que o filme selecionado não existe.<br>Confirme a sua seleção.</h2>';
+	}
+?>
+</body>
+</html>
